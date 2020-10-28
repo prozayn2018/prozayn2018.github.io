a51be8560f2ec9055977ad4c432c46c04faf21bc
Zayneel <zayneel@berkeley.edu>
2020-10-27 18:29:55 -0700

    second push cart not working yet

--- assets/js/page_1_landing.js
+++ assets/js/page_1_landing.js
@@ -16,6 +16,34 @@
     }
   });
 
+
+
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+      }, false)
+    })
+  }, false)
+}())
+
+
+
+
+
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 21;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
@@ -172,6 +200,8 @@
     loop: true,
     items: 1
   });
+
+
 
   // Init AOS
   function aos_init() {
